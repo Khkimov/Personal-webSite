a599e4b790b6ad51a7132fbc061043d330aac091
Ilyas <khakim198765@gmail.com>
2022-07-05 21:30:28 +0300

    Create experience

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,9 +1,54 @@
 import React from 'react';
 import './About.css';
+import ME from '../../assets/I_am.jpeg';
+import { FaAward, FaUserGraduate } from 'react-icons/fa';
 
 function About() {
   return (
-    <section id="about">About</section>
+    <section id="about">
+      <h5>Go To Know</h5>
+      <h2>About Me</h2>
+
+      <div className="container about__container">
+        <div className="about__me">
+          <div className="about__me-image">
+            <img src={ME} alt="About" />
+          </div>
+        </div>
+
+        <div className="about__content">
+          <div className="about__cards">
+            <article className="about__card">
+              <FaAward className="about__icon" />
+              <h5>Опыт</h5>
+              <small>1+ год Web-разработки</small>
+            </article>
+
+            <article className="about__card">
+              <FaUserGraduate className="about__icon" />
+              <h5>Образование</h5>
+              <small>Высшее</small>
+            </article>
+
+            <article className="about__card">
+              <FaAward className="about__icon" />
+              <h5>Курсы по программированию</h5>
+              <small>Основы WEB-разработки</small>
+              <small>Frontend-разработчик</small>
+              <small>Fullstack-разработчик</small>
+            </article>
+          </div>
+
+          <p>
+            Hi, my name is Ilyas! I am engaged in WEB development. I started with the layout of simple websites. 
+            I became interested in programming and for this I started learning languages (JavaScript, Node.js ). 
+            I have experience working in a team. I am constantly building up my programming skills.
+          </p>
+
+          <a href="#contact" className="btn btn-primary">Let`s Talk</a>
+        </div>
+      </div>
+    </section>
   );
 }
 
--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -1,9 +1,111 @@
 import React from 'react';
 import './Experience.css';
+import { BsFillPatchCheckFill } from 'react-icons/bs';
 
 function Experience() {
   return (
-    <section id="experience">Experience</section>
+    <section id="experience">
+      <h5>What Skills I Have</h5>
+      <h2>My Experience</h2>
+
+      <div className="container experience__container">
+        <div className="experience__frontend">
+          <h3>Frontend Development</h3>
+          <div className="experience__content">
+            <article className="experience__details">
+              <BsFillPatchCheckFill className="experience__details-icon" />
+              <div>
+                <h4>HTML</h4>
+                <small className="text-light">Experienced</small>
+              </div>
+            </article>
+            <article className="experience__details">
+              <BsFillPatchCheckFill className="experience__details-icon" />
+              <div>
+                <h4>CSS</h4>
+                <small className="text-light">Experienced</small>
+              </div>
+            </article>
+            <article className="experience__details">
+              <BsFillPatchCheckFill className="experience__details-icon" />
+              <div>
+                <h4>SCSS</h4>
+                <small className="text-light">Intermediate</small>
+              </div>
+            </article>
+            <article className="experience__details">
+              <BsFillPatchCheckFill className="experience__details-icon" />
+              <div>
+                <h4>JavaScript</h4>
+                <small className="text-light">Experienced</small>
+              </div>
+            </article>
+            <article className="experience__details">
+              <BsFillPatchCheckFill className="experience__details-icon" />
+              <div>
+                <h4>React</h4>
+                <small className="text-light">Experienced</small>
+              </div>
+            </article>
+            <article className="experience__details">
+              <BsFillPatchCheckFill className="experience__details-icon" />
+              <div>
+                <h4>Redux</h4>
+                <small className="text-light">Experienced</small>
+              </div>
+            </article>
+          </div>
+        </div>
+
+        <div className="experience__backend">
+          <h3>Backend Development</h3>
+          <div className="experience__content">
+            <article className="experience__details">
+              <BsFillPatchCheckFill className="experience__details-icon" />
+              <div>
+                <h4>Node.js</h4>
+                <small className="text-light">Intermediate</small>
+              </div>
+            </article>
+            <article className="experience__details">
+              <BsFillPatchCheckFill className="experience__details-icon" />
+              <div>
+                <h4>Express</h4>
+                <small className="text-light">Intermediate</small>
+              </div>
+            </article>
+            <article className="experience__details">
+              <BsFillPatchCheckFill className="experience__details-icon" />
+              <div>
+                <h4>PostgreSQL</h4>
+                <small className="text-light">Intermediate</small>
+              </div>
+            </article>
+            <article className="experience__details">
+              <BsFillPatchCheckFill className="experience__details-icon" />
+              <div>
+                <h4>Sequelize</h4>
+                <small className="text-light">Intermediate</small>
+              </div>
+            </article>
+            <article className="experience__details">
+              <BsFillPatchCheckFill className="experience__details-icon" />
+              <div>
+                <h4>Session</h4>
+                <small className="text-light">Intermediate</small>
+              </div>
+            </article>
+            <article className="experience__details">
+              <BsFillPatchCheckFill className="experience__details-icon" />
+              <div>
+              <h4>Jest</h4>
+              <small className="text-light">Experienced</small>
+              </div>
+            </article>
+          </div>
+        </div>
+      </div>
+    </section>
   );
 }
 
