99928f2b28d491c4768bd2395970c6b96198e4b0
Ilyas <khakim198765@gmail.com>
2022-07-06 22:28:41 +0300

    create form email

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,9 +1,54 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import './Contact.css';
+import { MdOutlineEmail } from 'react-icons/md';
+import { FaTelegram, FaWhatsapp } from 'react-icons/fa';
+import emailjs from 'emailjs-com';
 
 function Contact() {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_fabaf7g', 'template_6uyhkwe', form.current, 'bQLwenvccnFw8Fqbm')
+
+    e.target.reset()
+  };
+
   return (
-    <section id="contact">Contact</section>
+    <section id="contact">
+      <h5>Get In Touch</h5>
+      <h2>Contact Me</h2>
+
+      <div className="container contact__container">
+        <div className="contact__options">
+          <article className="contact__option">
+            <MdOutlineEmail className="contact__option-icon" />
+            <h4>Email</h4>
+            <h5>khakim198765@gmail.com</h5>
+            <a href="mailto:khakim198765@gmail.com" target="_blank" rel="noreferrer">Send a message</a>
+          </article>
+          <article className="contact__option">
+            <FaTelegram className="contact__option-icon" />
+            <h4>Telegram</h4>
+            <h5>@ilyas_khakimov</h5>
+            <a href="https://t.me/ilyas_khakimov" target="_blank" rel="noreferrer">Send a message</a>
+          </article>
+          <article className="contact__option">
+            <FaWhatsapp className="contact__option-icon" />
+            <h4>WhatsApp</h4>
+            <h5>+79080821181</h5>
+            <a href="https://api.whatsapp.com/send?phone=+79080821181" target="_blank" rel="noreferrer">Send a message</a>
+          </article>
+        </div>
+        <form ref={form} onSubmit={sendEmail}>
+          <input type="text" name="name" placeholder="Your Name" required/>
+          <input type="email" name="email" placeholder="Your Email" required/>
+          <textarea name="message" rows="7" placeholder="Your Message" required></textarea>
+          <button type="submit" className="btn btn-primary">Send Message</button>
+        </form>
+      </div>
+    </section>
   );
 }
 
--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,9 +1,51 @@
 import React from 'react';
 import './Portfolio.css';
+import Watch from '../../assets/watch-shop.png';
+import PlusKk from '../../assets/PlusKk.png';
+import Ascetic from '../../assets/Ascetic.png';
 
 function Portfolio() {
   return (
-    <section id="portfolio">Portfolio</section>
+    <section id="portfolio">
+      <h5>My Recent Work</h5>
+      <h2>Portfolio</h2>
+
+      <div className="container portfolio__container">
+        <article className="portfolio__item">
+          <div className="portfolio__item-image">
+            <img src={Watch} alt="Watch" />
+          </div>
+
+          <h3>portfolio title</h3>
+          <div className="portfolio__item-cta">
+            <a href="#ff" className="btn" target="_blank">Github</a>
+            <a href="#ff" className="btn btn-primary" target="_blank">Github</a>
+          </div>
+        </article>
+        <article className="portfolio__item">
+          <div className="portfolio__item-image">
+            <img src={PlusKk} alt="Watch" />
+          </div>
+
+          <h3>portfolio title</h3>
+          <div className="portfolio__item-cta">
+            <a href="#ff" className="btn" target="_blank">Github</a>
+            <a href="#ff" className="btn btn-primary" target="_blank">Github</a>
+          </div>
+        </article>
+        <article className="portfolio__item">
+          <div className="portfolio__item-image">
+            <img src={Ascetic} alt="Watch" />
+          </div>
+
+          <h3>portfolio title</h3>
+          <div className="portfolio__item-cta">
+            <a href="#ff" className="btn" target="_blank">Github</a>
+            <a href="#ff" className="btn btn-primary" target="_blank">Github</a>
+          </div>
+        </article>
+      </div>
+    </section>
   );
 }
 
